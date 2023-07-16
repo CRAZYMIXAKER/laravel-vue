e3534b9b7c59300a0311282f24f9e620af54260a
Mikhail Makavetski <zombisuki@gmail.com>
2023-07-16 20:24:41 +0200

    add database logic and models for it

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,27 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\CategoryRecipe;
+use App\Models\IngredientRecipe;
+use App\Models\Recipe;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(CategorySeeder::class);
+        $this->call(IngredientsSeeder::class);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(10)->create();
+        Recipe::factory(50)->create();
+        CategoryRecipe::factory(50)->create();
+        IngredientRecipe::factory(300)->create();
     }
+
 }
